7bff337dfd23eefff56159b726be6850cb04d9c0
Adelito <adelitobf@gmail.com>
2024-09-07 22:22:21 -0300

    Melhoria na arquitetura para fácil compreensão dos arquivos necessários para geração das massas de testes

--- cypress/e2e/step_test_definitions/massas/admin/gerarMassasAdmin.spec.js
+++ cypress/e2e/step_test_definitions/massas/admin/gerarMassasAdmin.spec.js
@@ -1,5 +1,5 @@
 import { Given, When } from "@badeball/cypress-cucumber-preprocessor";
-import { utilsPages } from "@pages/utils/utilsPages";
+import { testDataStrategy } from "@utils/massas/testDataStrategy";
 
 Given("que preciso gerar {string} perfis de administradores", (quantidade) => {
   cy.wrap(quantidade).as('quantidade');
@@ -7,5 +7,5 @@ Given("que preciso gerar {string} perfis de administradores", (quantidade) => {
 
 When("eu gerar os perfis de administradores", function () {
   const quantidade = this.quantidade;
-  utilsPages.gerarMassasAdmin(quantidade);
+  testDataStrategy.gerarMassasAdmin(quantidade);
 });
